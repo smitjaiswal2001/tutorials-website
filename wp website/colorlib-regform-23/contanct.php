f73189036e8e0204f92fdc09192b5c4358b0c653
smit jaiswal <32668118+smitjaiswal2001@users.noreply.github.com>
2021-04-04 14:49:43 +0530

    Add files via upload

---
+++ wp website/colorlib-regform-23/contanct.php
@@ -0,0 +1,10 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+echo 'Name:'.$name.'<br>';
+echo 'Email:'.$email.'<br>';
+echo 'your message:'.$message.'<br>';
+?>
+
+
